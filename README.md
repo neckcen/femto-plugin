d553fd702919a1b4ece4b4cfbd9ec4057bbbf4de
Sylvain Didelot <neck+github@eiky.net>
2014-03-03 12:22:30 +0000

    Add readme and demo.

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-femto-toc
-=========
+TOC plugin for Femto
+====================
 
-TOC plugin for Femto.
+A plugin to display a table of content.
+
+See demo page for usage.
+
+Requirements
+------------
+
+* [Femto](https://github.com/neckcen/femto)
+
+Installation
+------------
+Copy `toc.php` to `femto/plugins` then add _TOC_ to the list of enabled plugins
+in `index.php`:
+
+    $config['plugin_enabled'] = 'TOC';
+
+or
+
+    $config['plugin_enabled'] = 'Other_Plugin,TOC';
+
+It is recommended to add the following styles to your theme as well:
+
+    .toc {
+        border:1px solid #ddd;
+        float:right;
+        font-size:.9em;
+        margin:.5em 0 .5em .5em;
+        padding:.3em .5em;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+    .toc ol {
+        margin:0 0 0 .5em;
+    }
+    .toc > ol {
+        margin:0;
+    }
+    .toc ol li, .toc ol ol li, .toc ol ol ol li, .toc ol ol ol ol li {
+        list-style-type:none;
+        margin:0;
+    }
---
+++ toc.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace femto\plugin;
+
+/**
+ * A plugin for Femto that let you display a table of content.
+ *
+ * @author Sylvain Didelot
+ */
+class TOC {
+    /**
+     * If requested, find all titles in the page and produce a table of content.
+     *
+     * @param array $page Femto page.
+     */
+    public function page_complete(&$page) {
+        $match = array();
+        $re = '`(?:<p>)?%TOC(?::([0-9])(?:,([0-9]))?)?%(?:</p>)?`';
+        if(!preg_match($re, $page['content'], $match)) {
+            return;
+        }
+        $tag = $match[0];
+        $min = isset($match[1]) ? min(max($match[1], 1), 6) : 1;
+        $max = isset($match[2]) ? min(max($match[2], 1), 6) : 6;
+        if($min > $max) {
+            $max = $min;
+        }
+
+        $match = array();
+        $re = sprintf('`<h([%d-%d])>([^<]*)</h[%d-%d]>`', $min, $max, $min, $max);
+        preg_match_all($re, $page['content'], $match, PREG_SET_ORDER);
+        $current_level = $min;
+        $toc = '<nav class="toc"><ol>';
+        foreach ($match as $m) {
+            list($htag, $level, $title) = $m;
+            $id = preg_replace('`[^-_ 0-9a-zA-Z]`', '', $title);
+            $id = str_replace(' ', '_', trim($id));
+            $tag_id = sprintf(
+              '<h%d id="%s">%s</h%d>',
+              $level, $id, $title, $level
+            );
+            $page['content'] = str_replace($htag, $tag_id, $page['content']);
+            while($level > $current_level) {
+                $toc .= '<li><ol>';
+                $current_level++;
+            }
+            while($level < $current_level) {
+                $toc .= '</ol></li>';
+                $current_level--;
+            }
+            $toc .= sprintf(
+              '<li><a href="#%s">%s</a></li>',
+              $id, $title
+            );
+        }
+        while($current_level > $min) {
+            $toc .= '</ol></li>';
+            $current_level--;
+        }
+        $toc .= '</ol></nav>';
+        $page['content'] = str_replace($tag, $toc, $page['content']);
+    }
+}
